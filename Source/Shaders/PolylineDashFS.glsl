d7becf761f891aa508bb37f46782613234089b48
Tim Moore <timoore33@gmail.com>
2016-10-04 08:21:06 +0200

    Hacked up example

---
+++ Source/Shaders/PolylineDashFS.glsl
@@ -0,0 +1,14 @@
+varying vec2 v_st;
+
+void main()
+{
+    czm_materialInput materialInput;
+    
+    materialInput.s = v_st.s;
+    materialInput.st = v_st;
+    materialInput.str = vec3(v_st, 0.0);
+    // float a = mod(v_st.t,1.0);
+    float a = .1;
+    czm_material material = czm_getMaterial(materialInput);
+    gl_FragColor = vec4(material.diffuse + material.emission, material.alpha * a);
+}
