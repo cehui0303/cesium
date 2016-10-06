2dbe8c09addb023a906756bd1c08c4a739ef4326
Tim Moore <Tim.Moore@fei.com>
2016-10-06 18:35:07 +0200

    more screwing around

--- Source/Shaders/Materials/PolylineDashMaterial.glsl
+++ Source/Shaders/Materials/PolylineDashMaterial.glsl
@@ -7,8 +7,9 @@ czm_material czm_getMaterial(czm_materialInput materialInput)
     czm_material material = czm_getDefaultMaterial(materialInput);
 
     vec2 st = materialInput.st;
-    float dash = smoothstep(.4, .6, mod(st.s * 10));
-    material.alpha = dash * color.a;
+    float dash = smoothstep(.4, .6, mod(st.s * 10, 1.));
+    material.diffuse = vec(1.0, 0.0, 1.0);
+    material.alpha = dash;
 
     return material;
 }
