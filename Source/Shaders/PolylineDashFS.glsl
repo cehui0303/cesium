267fc36524499251dcc46126177e82ff287ad86f
Tim Moore <timoore33@gmail.com>
2016-10-13 20:41:13 +0200

    Dash demo

--- Source/Shaders/PolylineDashFS.glsl
+++ Source/Shaders/PolylineDashFS.glsl
@@ -8,7 +8,8 @@ void main()
     materialInput.st = v_st;
     materialInput.str = vec3(v_st, 0.0);
     czm_material material = czm_getMaterial(materialInput);
-    float dash = smoothstep(.475, .525, mod(st.s * 10., 1.));
-    material.alpha = dash;
-    gl_FragColor = vec4(material.diffuse + material.emission, material.alpha) * a;
+    float omega = mod(v_st.s * 10., 1.);
+    float dash = smoothstep(.475, .525, omega)*(1. - smoothstep(.95, 1., omega));
+    material.alpha = material.alpha * dash;
+    gl_FragColor = vec4(material.diffuse + material.emission, material.alpha);
 }
