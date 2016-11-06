511b28c375f31fa8fc7f594bb28b366e411f1890
Tim Moore <timoore33@gmail.com>
2016-11-06 23:54:37 +0100

    scale arc length
    Using the actual arc length in meters seems to be overflowing floats in
    the fragment shader.

--- Source/Shaders/Materials/PolylineDashMaterial.glsl
+++ Source/Shaders/Materials/PolylineDashMaterial.glsl
@@ -1,3 +1,5 @@
+precision highp float;
+
 uniform vec4 color;
 uniform float duty;
 uniform float dashLength;
@@ -9,8 +11,11 @@ czm_material czm_getMaterial(czm_materialInput materialInput)
     czm_material material = czm_getDefaultMaterial(materialInput);
 
     vec2 st = materialInput.st;
-    float omega = fract(st.s / dashLength);
-    float dash = smoothstep(duty - .05, duty, omega)*(1. - smoothstep(.95, 1., omega));
+    // float omega = fract(st.s / dashLength);
+    // float dash = smoothstep(duty - .05, duty, omega)*(1. - smoothstep(.95, 1., omega));
+    // float dash = omega;
+    // float dash = clamp(st.s / 2000000.0, 0.0, 1.0);
+    float dash = clamp(st.s, 0.0, 1.0);
     material.emission = color.rgb;
     material.alpha = dash * color.a;
 
