4bc4609ef133f98d7f47cec464078555a7dc9da6
Tim Moore <timoore33@gmail.com>
2016-11-06 23:54:37 +0100

    debugging hacks

--- Source/Shaders/Materials/PolylineDashMaterial.glsl
+++ Source/Shaders/Materials/PolylineDashMaterial.glsl
@@ -9,8 +9,10 @@ czm_material czm_getMaterial(czm_materialInput materialInput)
     czm_material material = czm_getDefaultMaterial(materialInput);
 
     vec2 st = materialInput.st;
-    float omega = fract(st.s / dashLength);
-    float dash = smoothstep(duty - .05, duty, omega)*(1. - smoothstep(.95, 1., omega));
+    // float omega = fract(st.s / dashLength);
+    // float dash = smoothstep(duty - .05, duty, omega)*(1. - smoothstep(.95, 1., omega));
+    // float dash = omega;
+    float dash = clamp(st.s / 2000000.0, 0.0, 1.0);
     material.emission = color.rgb;
     material.alpha = dash * color.a;
 
