1b65a3f35d3966c26c403d2bbdf92955620e7e8a
Jason Beverage <jasonbeverage@gmail.com>
2016-11-23 22:13:02 -0500

    Attempt to fix original Cesium Polyline rendering code by including an arcLength attribute instead of hijacking the texture coordinates

--- Source/Shaders/Appearances/PolylineMaterialAppearanceVS.glsl
+++ Source/Shaders/Appearances/PolylineMaterialAppearanceVS.glsl
@@ -7,23 +7,26 @@ attribute vec3 nextPosition3DLow;
 attribute vec2 expandAndWidth;
 attribute vec2 st;
 attribute float batchId;
+attribute float arcLength;
 
 varying float v_width;
 varying vec2 v_st;
+varying float v_arcLength;
 
-void main() 
+void main()
 {
     float expandDir = expandAndWidth.x;
     float width = abs(expandAndWidth.y) + 0.5;
     bool usePrev = expandAndWidth.y < 0.0;
-    
+
     vec4 p = czm_computePosition();
     vec4 prev = czm_computePrevPosition();
     vec4 next = czm_computeNextPosition();
-    
+
     v_width = width;
     v_st = st;
-    
+    v_arcLength = arcLength;
+
     vec4 positionWC = getPolylineWindowCoordinates(p, prev, next, expandDir, width, usePrev);
     gl_Position = czm_viewportOrthographic * positionWC;
 }
